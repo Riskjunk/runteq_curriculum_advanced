b0198eb5141f4320f9bbdd85313d5e022a7f2268
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-09-03 15:34:26 +0900

    アクション権限の調整

--- app/policies/category_policy.rb
+++ app/policies/category_policy.rb
@@ -1,2 +1,17 @@
 class CategoryPolicy < TaxonomyPolicy
+  def index?
+    user.admin? || user.editor?
+  end
+  
+  def edit?
+    user.admin? || user.editor?
+  end
+
+  def update?
+    user.admin? || user.editor?
+  end
+
+  def destroy?
+    user.admin? || user.editor?
+  end
 end
