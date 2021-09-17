be2ac8e13ea5d0d86d3326156b6945caab049a23
坂井良 <sakairyo@sakairyounoMacBook-Pro.local>
2021-09-17 17:42:11 +0900

    whenever による記事数一覧のメール送信5

--- app/mailers/article_mailer.rb
+++ app/mailers/article_mailer.rb
@@ -1,9 +1,4 @@
 class ArticleMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.article_mailer.report_summary.subject
-  #
   def report_summary
     @published_article_count = Article.published.count
     @articles_published_at_yesterday = Article.published_at_yesterday
